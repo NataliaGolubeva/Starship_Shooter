c993af04cf527e52f4ca8a4bae79efe1b81521c1
NataliaGolubeva <nataliagol787@gmail.com>
2021-02-09 08:57:10 +0100

    Initial commit

---
+++ finish/new_play.php
@@ -0,0 +1,74 @@
+<?php
+
+class Ship
+{
+public $name;
+public $weaponPower = 0;
+public $jediFactor = 0;
+public $strength = 0;
+
+public function  sayHello(){
+    echo 'HELLO!';
+}
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getNameAndSpecs($useShortFormat){
+        if($useShortFormat){
+            return sprintf(
+                '%s: %s/%s/%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+        else {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+    }
+
+}
+ function printShipSummary($someShip){
+    echo 'Ship name: ' .$someShip->name;
+    echo '<hr/>';
+     $someShip->sayHello();
+    echo '<hr/>';
+    echo $someShip->getName();
+    echo '<hr/>';
+    var_dump($someShip->weaponPower);
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(false);
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(true);
+}
+
+$myShip = new Ship();
+$myShip->name = 'Jedi Starship';
+$myShip->weaponPower = 10;
+
+$otherShip = new Ship();
+$otherShip->name = 'Imperial Shuttle';
+$otherShip->weaponPower = 5;
+$otherShip->strength = 50;
+
+
+printShipSummary($myShip);
+echo '<hr/>';
+echo '<hr/>';
+printShipSummary($otherShip);
+
+
+
+
